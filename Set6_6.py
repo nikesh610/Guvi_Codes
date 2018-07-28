f77bcb177cf9bec9e19681833644ca845f8f3549
Nikesh Pothabattula <30976883+nikesh610@users.noreply.github.com>
2018-07-28 13:29:51 +0530

    Update Set6_6.py

--- Set6_6.py
+++ Set6_6.py
@@ -1,5 +1,20 @@
 x=input()
-if(x.isalnum()):
+a=0
+n=0
+for i in range(len(x)):
+	if(a==0 and x[i].isalpha()):
+		a+=1
+		if(n>0 and a>0):
+			break
+	elif(n==0 and x[i].isdigit()):
+		n+=1
+		if(n>0 and a>0):
+			break
+	else:
+		pass
+if(a>0 and n>0):
 	print("yes")
 else:
 	print("no")
+	
+	
